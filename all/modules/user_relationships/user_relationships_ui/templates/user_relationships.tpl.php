a98f1671dd95165292f3f28ea22cc6083541f7fb
Justin <justin.hellings@gmail.com>
2013-04-26 13:34:00 +0100

    Applied security updates.

--- all/modules/user_relationships/user_relationships_ui/templates/user_relationships.tpl.php
+++ all/modules/user_relationships/user_relationships_ui/templates/user_relationships.tpl.php
@@ -8,10 +8,15 @@ if ($relationships) {
     $this_user_str  = $account->uid == $relationship->requestee_id ? 'requester' : 'requestee';
     $this_user      = $relationship->{$this_user_str};
 
+    if(!empty($relationship->extra_for_display)) {
+      $extra = $relationship->extra_for_display;
+    } else {
+      $extra = '';
+    }
     $row = array(
       theme('username', $this_user),
-      ur_tt("user_relationships:rtid:$relationship->rtid:name", $relationship->name) . ($relationship->is_oneway ? ($this_user_str == 'requestee' ? t(' (You to Them)') : t(' (Them to You)')) : NULL),
-      $relationship->extra_for_display,
+      check_plain(ur_tt("user_relationships:rtid:$relationship->rtid:name", $relationship->name)) . ($relationship->is_oneway ? ($this_user_str == 'requestee' ? t(' (You to Them)') : t(' (Them to You)')) : NULL),
+      $extra,
       $edit_access ? theme('user_relationships_remove_link', $account->uid, $relationship->rid) : '&nbsp;',
     );
     if (variable_get('user_relationships_show_user_pictures', 0)) {
